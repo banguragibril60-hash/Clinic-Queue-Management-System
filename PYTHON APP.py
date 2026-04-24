a29a15ad78f87aff0d872b844957e159286644f2
Gibril Bangura <banguragibril60@gmail.com>
2026-04-24 18:32:22 +0000

    Rename file to PROG103_Assignment1_GibrilBangura_905005708

---
+++ PYTHON APP.py
@@ -0,0 +1,7 @@
+# basic calculator
+num1 = int(input("Enter a number:"))
+num2 = int(input("Enter another number: "))
+result = (num1 + num2 )
+result = (num1 * num2 )
+result = (num1 / num2)
+print(result)
---
+++ Python Syntax.py
@@ -0,0 +1,41 @@
+#  I'm learning Python
+print("Hello World")
+num1, num2, num3 = 3, 4, 5
+print(num1, num2, num3)
+age = 18
+if  age < 18:
+    print("You are too young to attend this party.")
+else:
+    print("You required to attend this party.")
+    message = 34
+    print(message)
+
+    count = 8.6
+    print(count)
+    #count = count + 2
+    count += 3
+    print(count)
+person_name ='ALICE'
+print(person_name)
+alice_food = ("""no one should eat alice's food
+               it's ok to be awesome
+               stop !!""" )
+print(alice_food)
+POEM = ("""ALHAMDULILLAHI RABIL ALAMIN
+REMIND ME OF HOW I USED TO BE 5 YEARS AGO
+""")
+print(POEM)
+Freetown = "The capital city of Sierra Leone"
+Age = 20
+is_adult = age >= 18
+print(is_adult)
+
+#Truthly and Falsely Values
+print(bool(0))
+print(bool(1))
+print(bool(""))
+print(bool("Hi"))
+int(90.11)
+print(int(90.11))
+a = float(23.33)
+print(a)
